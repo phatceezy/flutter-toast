74c5604cd8e81398ce493f1cf35954fd465f9993
phatceezy <phatceezy@gmail.com>
2020-02-16 15:12:12 +1100

    Update flutter_toast.dart

--- lib/flutter_toast.dart
+++ lib/flutter_toast.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 class FlutterToast {
   static const MethodChannel _channel = MethodChannel('flutter_toast');
 
-  static showToast(String message, int bgcolor) {
+  static showToast(String message, double bgcolor) {
     _channel.invokeMethod("showToast", {"message": message, "bgcolor": bgcolor});
   }
 }
