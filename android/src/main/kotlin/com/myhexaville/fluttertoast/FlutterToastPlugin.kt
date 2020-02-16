ca1dfd85622590f195b77c07184f6cbbbad40133
phatceezy <phatceezy@gmail.com>
2020-02-16 13:52:24 +1100

    Update FlutterToastPlugin.kt

--- android/src/main/kotlin/com/myhexaville/fluttertoast/FlutterToastPlugin.kt
+++ android/src/main/kotlin/com/myhexaville/fluttertoast/FlutterToastPlugin.kt
@@ -6,6 +6,8 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import android.view.View
+import android.graphics.Color
 
 class FlutterToastPlugin(val registrar: Registrar) : MethodCallHandler {
   companion object {
